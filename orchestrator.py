da3da26e0b641597b4a867437f26d48ae9734c48
listra1321 <58926568+listra1321@users.noreply.github.com>
2026-03-21 04:37:29 +0700

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -7,7 +7,7 @@ class AgentOrchestrator:
         self.memory = ExampleMemory("data.jsonl")
         self.agent = UnifiedAgent(self.memory)
 
-    def run(self, text, caption, tujuan, destinasi):
+    def run(self, text, caption, tujuan, destination):
 
         destination = self.detect_destination(text)
 
