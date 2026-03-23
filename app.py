bd2cb49cf372f512bea8a946d0ff8e3571a1904a
listra1321 <58926568+listra1321@users.noreply.github.com>
2026-03-23 14:45:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ if st.button("🧠 Generate Storytelling & Kebijakan"):
         # MULTI-AGENT PROCESS
         # =========================
         with st.spinner("🤖 Menghasilkan storytelling dan kebijakan..."):
-            result = orch.run(text, caption, tujuan)
+            result = orch.run(text, caption, destination, tujuan)
 
         # =========================
         # OUTPUT BERSIH (NO CAPTION)
